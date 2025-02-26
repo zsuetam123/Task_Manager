954a18a886bacc08d00abcaf74f317e0f7b56784
Mateusz <r_marian@o2.pl>
2025-02-26 22:39:28 +0100

    Initial commit

---
+++ src/main/java/com/task_manager/task_manager/controller/RestController.java
@@ -0,0 +1,4 @@
+package com.task_manager.task_manager.controller;
+
+public class RestController {
+}
---
+++ src/main/java/com/task_manager/task_manager/service/TaskManagerService.java
@@ -0,0 +1,4 @@
+package com.task_manager.task_manager.service;
+
+public class TaskManagerService {
+}
